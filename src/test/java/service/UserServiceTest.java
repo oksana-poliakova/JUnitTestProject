2dbc50cef02f303c6cd7d836189a7b4e59cd4c8f
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2023-08-19 12:55:03 +0300

    Added the UserServiceTest

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,11 +1,130 @@
 package service;
 
+import dao.UserDao;
+import dto.CreateUserDto;
+import dto.UserDto;
+import entity.Gender;
+import entity.User;
+import entity.UserRole;
+import exception.ValidationException;
+import mapper.CreateUserMapper;
+import mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import validator.CreateUserValidator;
+import validator.Error;
+import validator.ValidatonResult;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 
 /**
  * @author oksanapoliakova on 19.08.2023
  * @projectName JUnitTestProject
  */
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-    
+    @Mock
+    private CreateUserValidator createUserValidator;
+    @Mock
+    private UserDao userDao;
+    @Mock
+    private CreateUserMapper createUserMapper;
+    @Mock
+    private UserMapper userMapper;
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void loginSuccess() {
+        User user = getUser();
+        UserDto userDto = getUserDto();
+        doReturn(Optional.of(user)).when(userDao).findByEmailAndPassword(user.getEmail(), user.getPassword());
+        doReturn(userDto).when(userMapper).map(user);
+
+        Optional<UserDto> actualResult = userService.login(user.getEmail(), user.getPassword());
+
+        assertThat(actualResult).isPresent();
+        assertThat(actualResult.get()).isEqualTo(userDto);
+    }
+
+    @Test
+    void loginFailed() {
+        doReturn(Optional.empty()).when(userDao).findByEmailAndPassword(any(), any());
+
+        Optional<UserDto> actualResult = userService.login("dummy", "123");
+
+        assertThat(actualResult).isEmpty();
+        verifyNoInteractions(userMapper);
+    }
+
+    @Test
+    void create() {
+        CreateUserDto createUserDto = getCreateUserDto();
+        User user = getUser();
+        UserDto userDto = getUserDto();
+        doReturn(new ValidatonResult()).when(createUserValidator).validate(createUserDto);
+        doReturn(user).when(createUserMapper).map(createUserDto);
+        doReturn(userDto).when(userMapper).map(user);
+
+        UserDto actualResult = userService.create(createUserDto);
+
+        assertThat(actualResult).isEqualTo(userDto);
+        verify(userDao).save(user);
+    }
+
+    @Test
+    void shouldThrowExceptionIfDtoInvalid() {
+        CreateUserDto createUserDto = getCreateUserDto();
+        ValidatonResult validationResult = new ValidatonResult();
+        validationResult.add(Error.of("invalid.role", "message"));
+        doReturn(validationResult).when(createUserValidator).validate(createUserDto);
+
+        assertThrows(ValidationException.class, () -> userService.create(createUserDto));
+        verifyNoInteractions(userDao, createUserMapper, userMapper);
+    }
+
+
+    private CreateUserDto getCreateUserDto() {
+        return CreateUserDto.builder()
+                .name("Ivan")
+                .email("test@gmail.com")
+                .password("123")
+                .birthday("2000-01-01")
+                .role(UserRole.USER.name())
+                .gender(Gender.MALE.name())
+                .build();
+    }
+
+    private UserDto getUserDto() {
+        return UserDto.builder()
+                .id(99)
+                .name("Ivan")
+                .email("test@gmail.com")
+                .gender(Gender.MALE)
+                .userRole(UserRole.USER)
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+    }
+
+    private User getUser() {
+        return User.builder()
+                .id(99)
+                .name("Ivan")
+                .gender(Gender.MALE)
+                .userRole(UserRole.USER)
+                .email("test@gmail.com")
+                .password("123")
+                .birthday(LocalDate.of(2000, 1, 1))
+                .build();
+    }
 }
